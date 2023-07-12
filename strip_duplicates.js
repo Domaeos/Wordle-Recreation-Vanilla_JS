cef077e09f9e32f5693d8a242d59f1c36f8843a1
Stian Hazelgrove <stian@Stians-Air-2.broadband>
2023-07-12 05:56:35 +0100

    fixed duplicate stripping

--- strip_duplicates.js
+++ strip_duplicates.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
-let arrayToExport;
 
-fs.readFile("backup.txt", "utf8", (err, data) => {
+fs.readFile("word_list.txt", "utf8", (err, data) => {
     if (err) {
         console.error(err);
         return;
@@ -15,30 +14,19 @@ fs.readFile("backup.txt", "utf8", (err, data) => {
             if (index > 3500) {
                 return;
             } else {
-                let thisWord = [];
-                value.split("").map((value, index, array) => {
-                    if (thisWord.includes(value)) {
-                        thisWord = [];
-                        return;
-                    } else {
-                        thisWord.push(value);
-                        if (index === array.length - 1) {
-                            trimmedWordArray.push(thisWord.join(""));
-                            thisWord = [];
-                        }
-                    }
-                });
+                let duplicates = [];
+                // for each char in the current word
+                for (const char of value) {
+                    // this returns out of the wordArray.forEach, so we skip to the next word
+                    if (duplicates.includes(char)) return;
+                    duplicates.push(char)
+                }
+
+                trimmedWordArray.push(value);
             }
         })
-
-        // No idea why certain letters are making their way in.. to be continued. 
-        // Should ave enough for now to write to new file for Wordle.
-        // Trimming nonsense results..
-        arrayToExport = trimmedWordArray.filter(x => {
-            return x.length === 5;
-        })
-        console.log(arrayToExport);
-        const JSONarray = JSON.stringify(arrayToExport);
+        
+        const JSONarray = JSON.stringify(trimmedWordArray);
         fs.writeFile('test.txt', JSONarray, { flag: 'a+' }, err => {});
     }
 });
