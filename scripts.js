2e1860b5664b3af90114e585ffae07058ad4e061
Ryan Murphy <ryanmurphy.ic@icloud.com>
2023-07-12 11:13:16 +0100

    Complete word list + added not a valid word checker

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 var request = new XMLHttpRequest();
+
+// Could change this to a simple include .js file with word_list as array
 request.open('GET', '/stripped_wordlist.txt', true);
 
 
@@ -27,7 +29,7 @@ request.onreadystatechange = function () {
         function keyPressed(event) {
 
             if (!letterRegex.test(event.key)) return;
-            
+
             // Not allowing words with duplicate letters.. yet?
             if (guessedWord.includes(event.key)) return;
 
@@ -53,98 +55,91 @@ request.onreadystatechange = function () {
             currentTile.classList.add("guessed");
             currentLetterPosition++;
 
-
             if (currentLetterPosition > 5) {
-                // Get current row element
-                amountOfGuesses++;
-
-                // Check if word exists ? Data file not big enough
-                // if (wordListArray.includes(guessedWord.join(""))) {
-
-                if (guessedWord.join("") === wordToGuess) {
-                    // Word is correct
-                    console.log("You win!");
-                    const letterBoxes = currentRow.querySelectorAll(".letterBox");
-                    letterBoxes.forEach(letterBox => letterBox.classList.add("correct"));
-                    endGame();
-
-                } else {
-                    // Check letters and reset guess
-                    wordGrid.classList.add("shake-horizontal");
-                    window.setTimeout(() => {
-                        wordGrid.classList.remove("shake-horizontal");
-                    }, 600);
 
 
-                    guessedWord.map((value, index) => {
+                    if (guessedWord.join("") === wordToGuess) {
+                        // Word is correct
+                        console.log("You win!");
+                        const letterBoxes = currentRow.querySelectorAll(".letterBox");
+                        letterBoxes.forEach(letterBox => letterBox.classList.add("correct"));
+                        endGame();
 
-                        const currentTile = currentRow.querySelector(`[data-id="${index}"]`)
-                        const currentKey = keyboardMarker.querySelector(`#${value}_key`);
+                    } else if (wordListArray.includes(guessedWord.join(""))) {
+                        amountOfGuesses++;
+                        // Check letters and reset guess
+                        wordGrid.classList.add("shake-horizontal");
+                        window.setTimeout(() => {
+                            wordGrid.classList.remove("shake-horizontal");
+                        }, 600);
 
-                        if (wordToGuess.includes(value)) {
 
-                            // In correct place? 
-                            if (wordToGuess.indexOf(value) === index) {
-                                currentTile.classList.add("correct");
-                                // Check key is already set the class
-                                if (!currentKey.classList.contains("correct")) {
-                                    currentKey.classList.add("correct");
-                                    currentKey.classList.add("pulse-out");
+                        guessedWord.map((value, index) => {
+
+                            const currentTile = currentRow.querySelector(`[data-id="${index}"]`)
+                            const currentKey = keyboardMarker.querySelector(`#${value}_key`);
+
+                            if (wordToGuess.includes(value)) {
+
+                                // In correct place? 
+                                if (wordToGuess.indexOf(value) === index) {
+                                    currentTile.classList.add("correct");
+                                    // Check key is already set the class
+                                    if (!currentKey.classList.contains("correct")) {
+                                        currentKey.classList.add("correct");
+                                        currentKey.classList.add("pulse-out");
+                                    }
+                                } else {
+                                    currentTile.classList.add("wrong_place");
+                                    if (!currentKey.classList.contains("wrong_place")) {
+                                        currentKey.classList.add("wrong_place");
+                                    }
                                 }
                             } else {
-                                currentTile.classList.add("wrong_place");
-                                if (!currentKey.classList.contains("wrong_place")) {
-                                    currentKey.classList.add("wrong_place");
+
+                                currentTile.classList.add("wrong");
+                                if (!currentKey.classList.contains("wrong")) {
+                                    currentKey.classList.add("wrong");
                                 }
                             }
-                        } else {
 
-                            currentTile.classList.add("wrong");
-                            if (!currentKey.classList.contains("wrong")) {
-                                currentKey.classList.add("wrong");
-                            }
+                        });
+
+
+                        currentLetterPosition = 1;
+                        guessedWord = [];
+                        if (amountOfGuesses > 6) {
+
+                            // NO MORE GUESSES
+                            endGame();
                         }
-
-                    });
-
-
-                    currentLetterPosition = 1;
-                    guessedWord = [];
-                    if (amountOfGuesses > 6) {
-
-                        // NO MORE GUESSES
-                        endGame();
+                    } else {
+                        console.log("Not a valid word");
                     }
+
                 }
 
 
-                // } else {
-                // Word not in array
-                //  }
-
-            }
-
-
-            function endGame() {
-                document.removeEventListener("keyup", keyPressed);
-            }
-
-            function deleteLastLetter() {
-                let currentRow = document.querySelector(`#row${amountOfGuesses}`);
-                let guessedElements = currentRow.querySelectorAll(".guessed")
-                if (guessedElements.length) {
-
-                    let lastGuessElement = guessedElements[guessedElements.length - 1]
-                    lastGuessElement.classList.remove("guessed");
-                    lastGuessElement.textContent = "";
-
-                    guessedWord.pop();
-                    currentLetterPosition--;    
+                function endGame() {
+                    document.removeEventListener("keyup", keyPressed);
                 }
 
+                function deleteLastLetter() {
+                    let currentRow = document.querySelector(`#row${amountOfGuesses}`);
+                    let guessedElements = currentRow.querySelectorAll(".guessed")
+                    if (guessedElements.length) {
+
+                        let lastGuessElement = guessedElements[guessedElements.length - 1]
+                        lastGuessElement.classList.remove("guessed");
+                        lastGuessElement.textContent = "";
+
+                        guessedWord.pop();
+                        currentLetterPosition--;
+                    }
+
+                }
             }
         }
-    }
 
-}
-request.send();
+    }
+    request.send();
--- strip_duplicates.js
+++ strip_duplicates.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-fs.readFile("backup.txt", "utf8", (err, data) => {
+fs.readFile("word_list.txt", "utf8", (err, data) => {
     if (err) {
         console.error(err);
         return;
@@ -10,27 +10,23 @@ fs.readFile("backup.txt", "utf8", (err, data) => {
         const wordArray = data.split(/\r|\n/);
         const trimmedWordArray = [];
 
-        wordArray.forEach((value, index) => {
-            if (index > 3500) {
-                return;
-            } else {
-                let thisWord = [];
-                value.split("").some((value, index, array) => {
-                    if (thisWord.includes(value)) {
+        wordArray.forEach(value => {
+            let thisWord = [];
+            value.split("").some((value, index, array) => {
+                if (thisWord.includes(value)) {
+                    thisWord = [];
+                    return true;
+                } else {
+                    thisWord.push(value);
+                    if (index === array.length - 1) {
+                        trimmedWordArray.push(thisWord.join(""));
                         thisWord = [];
-                        return true;
-                    } else {
-                        thisWord.push(value);
-                        if (index === array.length - 1) {
-                            trimmedWordArray.push(thisWord.join(""));
-                            thisWord = [];
-                        }
                     }
-                });
-            }
+                }
+            });
         })
         console.log(trimmedWordArray);
         const JSONarray = JSON.stringify(trimmedWordArray);
-        fs.writeFile('stripped_wordlist.txt', JSONarray, { flag: 'a+' }, err => {});
+        fs.writeFile('stripped_wordlist.txt', JSONarray, { flag: 'a+' }, err => { });
     }
 }); 
